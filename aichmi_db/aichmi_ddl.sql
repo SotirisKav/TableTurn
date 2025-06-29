d153cb79f050802bca01c4e88a00b6ea560b52ca
SotirisKav <sotiriskavadakis@gmail.com>
2025-06-29 21:16:33 +0300

    changed constraints

--- aichmi_db/aichmi_ddl.sql
+++ aichmi_db/aichmi_ddl.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS aichmi;
 CREATE DATABASE aichmi;
-USE aichmi;
 
 DROP TABLE IF EXISTS wedding_dates;
 DROP TABLE IF EXISTS reservation;
@@ -15,6 +14,7 @@ CREATE TABLE customer (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(20)
+    CONSTRAINT customer_email_format CHECK (email ~* '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$')
 );
 
 CREATE TABLE venue (
@@ -39,28 +39,28 @@ CREATE TABLE hotel (
     name VARCHAR(100) NOT NULL,
     address VARCHAR(255) NOT NULL,
     area VARCHAR(50) NOT NULL,
-    transfer_price NUMERIC(5,2) DEFAULT 0
+    transfer_price NUMERIC(5,2) DEFAULT 0 CHECK (transfer_price >= 0)
 );
 
 CREATE TABLE tables (
     table_id SERIAL PRIMARY KEY,
     table_type TEXT NOT NULL CHECK(table_type IN ('standard', 'grass', 'anniversary')),
-    table_price NUMERIC(5,2) DEFAULT 0
+    table_price NUMERIC(5,2) DEFAULT 0 CHECK (table_price >= 0)
 );
 
 CREATE TABLE reservation (
     reservation_id SERIAL PRIMARY KEY,
-    reservation_date DATE NOT NULL,
+    reservation_date DATE NOT NULL CHECK (reservation_date >= CURRENT_DATE),
     reservation_time TIME NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     guests INT NOT NULL CHECK (guests > 0),
-    table_type TEXT NOT NULL CHECK(table_type IN ('standard', 'grass', 'anniversary')),
-    table_price NUMERIC(5,2) DEFAULT 0,
+    table_type TEXT NOT NULL CHECK (table_type IN ('standard', 'grass', 'anniversary')),
+    table_price NUMERIC(5,2) DEFAULT 0 CHECK (table_price >= 0),
     celebration_type TEXT,
     cake BOOLEAN DEFAULT FALSE,
-    cake_price NUMERIC(5,2),
+    cake_price NUMERIC(5,2) CHECK (cake_price >= 0),
     flowers BOOLEAN DEFAULT FALSE,
-    flowers_price NUMERIC(5,2),
+    flowers_price NUMERIC(5,2) CHECK (flowers_price >= 0),
     hotel_name TEXT,
     hotel_id INT,
     customer_id INT NOT NULL,
@@ -70,9 +70,9 @@ CREATE TABLE reservation (
 
 CREATE TABLE wedding_dates (
     wedding_date_id SERIAL PRIMARY KEY,
-    wedding_date DATE NOT NULL,
+    wedding_date DATE NOT NULL CHECK (wedding_date >= CURRENT_DATE),
     customer_id INT NOT NULL,
     reservation_id INT NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id)
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ON DELETE CASCADE,
+    FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id) ON DELETE CASCADE
 );
